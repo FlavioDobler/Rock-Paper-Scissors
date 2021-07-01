bba1f05b49324b968c9c161f73a98f1ff0d6ba19
Flavio Dobler <flaviodobler2@gmail.com>
2021-07-01 12:58:27 -0300

    JS logic

--- app.js
+++ app.js
@@ -10,13 +10,47 @@ const scissors_div = document.getElementById("s");
 
 function getComputerChoice(){
     const choices = [r,p,s];
-    console.log(Math.random());
+    const randomNumber = (Math.floor(Math.random()*3));
+    return choices[randomNumber];
 }
 
-getComputerChoice();
+function win(){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML=computerScore;
+
+}
+
+function lose(){
+
+}
+
+function draw(){
+
+}
+
 
 function game(userChoice){
-    console.log(userChoice)
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("USER WINS.");
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            console.log("USER LOSES.");
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("ITS A DRAW.");
+        break;
+        
+    
+    }
 }
 
 function main(){
